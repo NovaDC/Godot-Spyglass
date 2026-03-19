7e1d4b7fcbb5c73673e21bfad0ddddd06ed1b171
NovaDC <164206531+NovaDC@users.noreply.github.com>
2026-03-19 13:32:21 -0500

    Assist script documentation loading
    Makes the scripts automatically load in the editor when appropriate, to make the editor documentation viewer more reliable for those scripts.

--- addons/spyglass/plugin.gd
+++ addons/spyglass/plugin.gd
@@ -5,6 +5,18 @@ const PLUGIN_NAME := "Spyglass"
 
 const PLUGIN_ICON:Texture2D = preload("./icon.svg")
 
+const _ENSURE_SCRIPT_DOCS:Array[Script] = [
+    preload("./spyglass.gd")
+]
+
+# Every once ands a while the script docs simply refuse to update properly.
+# This nudges the docs into a ensuring that the important scripts added by
+# this addon are actually loaded.
+func _ensure_script_docs() -> void:
+	var edit := EditorInterface.get_script_editor()
+	for scr in _ENSURE_SCRIPT_DOCS:
+		edit.update_docs_from_script(scr)
+
 func _get_plugin_name() -> String:
 	return PLUGIN_NAME
 
